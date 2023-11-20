0542475a4b0f2d34e3cc5927833b9e1784362c46
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-11-20 12:55:48 -0300

    wip: amazon account login

--- CaptureReceipt/Classes/Account/Account.swift
+++ CaptureReceipt/Classes/Account/Account.swift
@@ -11,55 +11,22 @@ import BlinkEReceipt
 
 /// A Swift class representing an account, which can be of different types, such as email or retailer.
 public class Account {
-    /// The type of the account, such as email or retailer, represented by an AccountCommon object.
-    let accountType: AccountCommon
+    /// The type of the account, such as email or retailer, represented by an AccountType object.
+    let accountType: AccountType
     /// The username associated with the account.
     let user: String
-    /// The password associated with the account (optional).
-    let password: String?
     /// A flag indicating whether the account has been verified (optional).
     var isVerified: Bool?
     
     /// Initializes an Account object with the specified properties.
     ///
     /// - Parameters:
-    ///   - accountType: An AccountCommon object representing the type and source of the account.
+    ///   - accountType: An AccountType object representing the type and source of the account.
     ///   - user: The username associated with the account.
-    ///   - password: The password associated with the account (optional).
     ///   - isVerified: A flag indicating whether the account has been verified (optional).
-    init(accountType: AccountCommon, user: String, password: String?, isVerified: Bool?) {
+    init(accountType: AccountType, user: String, password: String?, isVerified: Bool?) {
         self.accountType = accountType
         self.user = user
-        self.password = password
         self.isVerified = isVerified ?? false
-    }
-    
-    /// Initializes an Account object for an email account with the specified provider and email address.
-    ///
-    /// - Parameters:
-    ///   - provider: The BREReceiptProvider representing the email provider.
-    ///   - email: The email address associated with the account.
-    ///   - password: The password associated with the account (optional).
-    init(provider: BREReceiptProvider, email: String, password: String? = nil){
-        let emailEnum = EmailEnum.allCases.first(where: {value in
-            value.toBREReceiptProvider() == provider
-        })
-        self.accountType = AccountCommon.defaults.first(where: {key, value in
-            value.source == emailEnum?.rawValue
-        })!.value
-        self.user = email
-        self.password = password
-    }
-
-    
-    /// Converts the Account object to a PluginCallResultData dictionary for use in plugin calls.
-    ///
-    /// - Returns: A PluginCallResultData dictionary representing the Account object's properties.
-    func toString() -> [String : Any] {
-        return [
-            "username" : self.user,
-            "source" : self.accountType.source,
-            "verified" : self.isVerified ?? false
-        ]
     }
 }
